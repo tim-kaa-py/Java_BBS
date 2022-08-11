76e6b24abf12d3a04170fbcdc7e72e09800688c4
MarkusSchulze <markus.schulze@adweko.com>
2022-08-11 18:40:15 +0200

    new exercise + gitignore

--- Excercises/src/main/java/IHaveThePower.java
+++ Excercises/src/main/java/IHaveThePower.java
@@ -4,10 +4,17 @@ public class IHaveThePower {
      * <p>
      * Now, another case. We have the number 1245678. We want to know the 5th power, closest and higher than that number. The value will be 1419857.
      * <p>
-     * We need a function find_next_power ( findNextPower in JavaScript, CoffeeScript and Haskell), that receives two arguments, a value val, and the exponent of the power, pow_, and outputs the
+     * We need a function find_next_power that receives two arguments, a value val, and the exponent of the power, pow_, and outputs the
      * value that we want to find.
      */
-    static int find_next_power(int number, int power) {
-        return 0;
+    static double find_next_power(double number, double power) {
+        if (number <= 0 | power <= 0) return 0;
+
+        double counter = 1;
+        do {
+            final double result = Math.pow(counter, power);
+            if (result > number) return result;
+            counter++;
+        } while (true);
     }
 }
