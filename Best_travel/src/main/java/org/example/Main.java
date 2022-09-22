f386da23056553e70cf7fade4c91ee0dd438c8b7
tim-kaa-py <99023354+tim-kaa-py@users.noreply.github.com>
2022-09-22 11:21:43 +0200

    confluence test commit

--- Best_travel/src/main/java/org/example/Main.java
+++ Best_travel/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class Main {
+    //Will this line of code be in the test file on confluence???
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
